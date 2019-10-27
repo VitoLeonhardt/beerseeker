b7a72b32e51d93ee7ade1d5ece8ac37f564615bb
VitoLeonhardt <witold.chojnowski@gmail.com>
2019-10-27 16:31:45 +0100

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Image } from "semantic-ui-react";
+import { Table, Image, Pagination } from "semantic-ui-react";
 import logo from "./beerseker.jpg";
 import './App.css';
 
@@ -8,6 +8,7 @@ import { getBeers } from "./utils.js";
 
 function App() {
   const [beers, setBeers] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     // this function is because useEffect's function cannot be async by itself.
@@ -19,7 +20,17 @@ function App() {
     if(beers.length === 0) {
       init();
     }
-  }, [beers])
+  }, [beers]);
+
+  useEffect(() => {
+    // this function is because useEffect's function cannot be async by itself.
+    const updateBeers = async () => {
+      const response = await getBeers({ page, per_page: 10 });
+      console.log(beers);
+      setBeers(response);
+    };
+    updateBeers();
+  }, [page])
   return (
     <div>
       <Image className="logo-image" src={logo}/>
@@ -35,6 +46,16 @@ function App() {
         {beers.map((beer) => <Beer beer={beer}/>)}
       </Table.Body>
       </Table>
+        <Pagination
+          boundaryRange={0}
+          defaultActivePage={1}
+          onPageChange={(event, data) => setPage(data.activePage)}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          siblingRange={1}
+          totalPages={50}
+        />
     </div>
   );
 }
