c3ed0b46bc7637fdafd6cf55ffade9f9598c86bf
VitoLeonhardt <witold.chojnowski@gmail.com>
2019-10-29 21:47:08 +0100

    Filled the modal with information

--- src/Beer.js
+++ src/Beer.js
@@ -5,7 +5,7 @@ import "./Beer.css";
 export const Beer = ({ beer, onClick }) => { // my eyes hurt from looking at the letters b e e r
     const { name, image_url, first_brewed } = beer;
     return (
-    <Table.Row onClick={() => onClick(beer)}>
+    <Table.Row onClick={() => onClick(beer)} className={"beer-row"}>
         <Table.Cell>{name}</Table.Cell>
         <Table.Cell><Image className="beer-image" src={image_url} alt="beer"/></Table.Cell>
         <Table.Cell>{first_brewed}</Table.Cell>
--- src/BeerDetails.js
+++ src/BeerDetails.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Modal, Header } from "semantic-ui-react";
+import { Modal, Header, Grid, Image } from "semantic-ui-react";
 
-export const BeerDetails = ({ beer, onClose }) => (
+import "./BeerDetails.css";
+
+export const BeerDetails = ({ beer, onClose }) => 
+    (
     <Modal
         open={!!beer}
         onClose={onClose}
@@ -11,6 +14,23 @@ export const BeerDetails = ({ beer, onClose }) => (
         <>
         <Header content={beer.name || ""} />
         <Modal.Content>
+            <Grid>
+                <Grid.Column width={3}>
+                    <Image src={beer.image_url}/>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                    <div className="beer-details-text"><i>{beer.description}</i></div>
+                    <div className="beer-details-text"><b>ABV: </b>{beer.abv}</div>
+                    <div className="beer-details-text"><b>Volume: </b>{beer.volume.value + " " + beer.volume.unit} </div>
+                    <div className="beer-details-text"><b>Food pairings: </b>
+                        <ul>
+                            <>
+                            {beer.food_pairing.map((pairing) => <li>{pairing}</li>)}
+                            </>
+                        </ul>
+                    </div>
+                </Grid.Column>
+            </Grid>
         </Modal.Content>
         </>}
     </Modal>
